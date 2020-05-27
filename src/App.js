b68431f4cbdaf97ec788189ca48a7e0e611578bf
NateTheDev1 <nthnlrichards@gmail.com>
2020-05-26 21:11:26 -0400

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import AppStyles from "./styles/AppStyles";
+import { Container, Switch } from "@material-ui/core";
 
 const App = () => {
+  const classes = AppStyles();
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={classes.root}>
+      <header className={classes.header}>
+        <Container maxWidth="md">
+          <h1 className={classes.heading}>Social Media Dashboard</h1>
+          <p className={classes.followers}>Total Followers: 23, 004</p>
+        </Container>
+        <div className={classes.darkModeSwitch}>
+          <p className={classes.sub}>Dark Mode</p>
+          <Switch />
+        </div>
+      </header>
     </div>
   );
 };
---
+++ src/styles/AppStyles.js
@@ -0,0 +1,42 @@
+import { makeStyles } from "@material-ui/core";
+
+const AppStyles = makeStyles({
+  root: {
+    display: "flex",
+    height: "100%",
+  },
+  header: {
+    boxSizing: "border-box",
+    padding: "20px 1.25%",
+    margin: "0 auto",
+    backgroundColor: "hsl(232, 19%, 15%)",
+    color: "hsl(0, 0%, 100%)",
+    width: "100%",
+    height: "15vh",
+    borderBottomLeftRadius: "25px",
+    borderBottomRightRadius: "25px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  heading: {
+    fontSize: "2rem",
+    fontWeight: "700",
+    color: "hsl(0, 0%, 100%)",
+  },
+  followers: {
+    marginTop: "1.5%",
+    color: "hsl(228, 34%, 66%)",
+  },
+  darkModeSwitch: {
+    width: "20%",
+    display: "flex",
+    alignItems: "center",
+  },
+  sub: {
+    marginRight: "1%",
+    color: "hsl(228, 34%, 66%)",
+  },
+});
+
+export default AppStyles;
